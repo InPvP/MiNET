c4363dd9c801e61f43ba1b5c39a0b54e1138aa68
BrychanOdlum <brychan@me.com>
2017-02-22 15:17:22 +0000

    Minet: add EnderCrystal

---
+++ src/MiNET/MiNET/Entities/Projectiles/EnderPearl.cs
@@ -0,0 +1,19 @@
+﻿using MiNET.Worlds;
+
+namespace MiNET.Entities.Projectiles
+{
+	/*public class EnderPearl : Projectile
+	{
+		public EnderPearl(Player shooter, Level level) : base(shooter, 87, level, 0)
+		{
+			Width = 0.25;
+			Length = 0.25;
+			Height = 0.25;
+
+			Gravity = 0.03;
+			Drag = 0.01;
+
+			HealthManager.IsInvulnerable = true;
+		}
+	}*/
+}
---
+++ src/MiNET/MiNET/Entities/World/EnderCrystal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MiNET.Net;
+using MiNET.Utils;
+using MiNET.Worlds;
+
+namespace MiNET.Entities.World
+{
+	public class EnderCrystal : Entity
+	{
+
+		public EnderCrystal(Level level) : base(71, level)
+		{
+
+		}
+
+
+	}
+}
